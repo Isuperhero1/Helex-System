c6f3bb867b802bb250d9e15b691ee0044969c12c
Isuperhero1 <49760771+Isuperhero1@users.noreply.github.com>
2019-04-20 11:48:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,21 +33,9 @@ var ti={}
 ,spee={}
 ,attentions={};
 
-client.on('ready', function(){
-    var ms = 60000 ;
-    var setGame = ['&help'];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/barontube`);
-    }, ms);
+lient.user.setPresence({ game: { name: '&help' }, status: 'online' });
+bot.user.setGame('&help', 'https://www.twitch.tv/monstercat');
+
     console.log(` ????????? |> Name: ${client.user.username}`);
  console.log(` ????????? |> Servers: ${client.guilds.size}`);
  console.log(` ???????????????????? |> Members: ${client.users.size}`);
